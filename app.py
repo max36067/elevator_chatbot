1fe05371c028743cf716c5f5979364229d90298a
Max Huang <max36067@gmail.com>
2019-11-26 21:47:49 +0800

    make it better

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def handler_message(event):
             quick_reply=QuickReplyButton(action=PostbackAction(
                 label="了解了!",
                 data="capital_1"
-            ))))]
+            )))]
         )
         # line_bot_api.unlink_rich_menu_from_user(secret_file['self_user_id'])
         # 之後再把這行打開
